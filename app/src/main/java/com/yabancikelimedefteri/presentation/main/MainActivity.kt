c83ae3c33a868f5ab13b19567543d564fbfde1d6
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-02 16:13:50 +0300

    hilt application class created

--- app/src/main/java/com/yabancikelimedefteri/presentation/main/MainActivity.kt
+++ app/src/main/java/com/yabancikelimedefteri/presentation/main/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.yabancikelimedefteri.core.navigation.NavGraph
 import com.yabancikelimedefteri.core.ui.theme.YabanciKelimeDefteriTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
