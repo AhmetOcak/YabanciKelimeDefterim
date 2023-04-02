f7c2f78562f22142bcfaa90b425087f19a3f1357
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-02 16:14:50 +0300

    error feature added

--- app/src/main/java/com/yabancikelimedefteri/core/ui/component/CustomTextField.kt
+++ app/src/main/java/com/yabancikelimedefteri/core/ui/component/CustomTextField.kt
@@ -1,18 +1,26 @@
 package com.yabancikelimedefteri.core.ui.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yabancikelimedefteri.R
 
 @Composable
 fun CustomTextField(
     modifier: Modifier,
     value: String,
     onValueChange: (String) -> Unit,
-    labelText: String
+    labelText: String,
+    isError: Boolean = false
 ) {
     OutlinedTextField(
         modifier = modifier,
@@ -20,6 +28,26 @@ fun CustomTextField(
         onValueChange = onValueChange,
         label = { Text(text = labelText) },
         maxLines = 1,
-        shape = RoundedCornerShape(20)
+        shape = RoundedCornerShape(20),
+        isError = isError,
+        singleLine = true,
+        trailingIcon = if (isError) {
+            {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_baseline_error),
+                    contentDescription = "Bu alanı boş bırakamazsın",
+                    tint = MaterialTheme.colors.error
+                )
+            }
+        } else null
     )
+    if (isError) {
+        Text(
+            modifier = modifier.padding(top = 4.dp, start = 16.dp),
+            text = "Bu alanı boş bırakamazsın",
+            color = MaterialTheme.colors.error,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
