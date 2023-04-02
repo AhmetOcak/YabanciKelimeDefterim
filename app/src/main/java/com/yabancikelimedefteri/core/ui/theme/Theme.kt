7c805bdceefc7c19808652695be211b5e60c3632
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-02 14:36:26 +0300

    app theme set

--- app/src/main/java/com/yabancikelimedefteri/core/ui/theme/Theme.kt
+++ app/src/main/java/com/yabancikelimedefteri/core/ui/theme/Theme.kt
@@ -5,26 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = CustomOrange,
+    secondary = CustomOrange,
+    background = CustomBlack
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = CustomOrange,
+    secondary = CustomOrange,
+    surface = WhiteSmoke
 )
 
 @Composable
@@ -32,6 +24,20 @@ fun YabanciKelimeDefteriTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+
+    if (darkTheme) {
+        systemUiController.setStatusBarColor(
+            color = CustomDarkGray,
+            darkIcons = false
+        )
+    } else {
+        systemUiController.setStatusBarColor(
+            color = CustomDarkOrange,
+            darkIcons = false
+        )
+    }
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
