ea791b38c99c6915dcf7688cf6f60556b6585fb6
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-02 14:36:44 +0300

    typography set

--- app/src/main/java/com/yabancikelimedefteri/core/ui/theme/Type.kt
+++ app/src/main/java/com/yabancikelimedefteri/core/ui/theme/Type.kt
@@ -9,20 +9,19 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 20.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = FontFamily.Monospace,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
+
 )
